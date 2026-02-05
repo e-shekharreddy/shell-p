3d377a8f8e8d0eb2e3a19facd899de89e4529fb6
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-02-05 22:01:40 +0530

    upto date

---
+++ 03-veriables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+person1=vardhan # no space between values and =
+person2=thadhiya
+
+
+echo "$person1: hai $person2 how are you?"
+echo "$person2: fine $person1,what about you ?"
+echo "$person1: good,what are you doing?"
+echo "$person2: i am watching TV"
+
---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter user name::"
+read USER_NAME
+
+echo "User name is $USER_NAME"
+
+echo "please enter password::"
+read -s PASSWORD # -e is for to hide the password 
+
+read "password is $PASSWORD" #to see what password we enter
+
+
