679ecb1a86f02a3fcdb981b0ff07b3fed1e38f99
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-02-07 23:17:03 +0530

    upto date

--- 05-variables.sh
+++ 05-variables.sh
@@ -9,4 +9,4 @@ echo "please enter password::"
 read -s PASSWORD # -s is for to hide the password while entering the password
 
 
-echo "password is $PASSWORD" #to see what password we enter
+read "password is $PASSWORD" #to see what password we enter
