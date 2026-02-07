df38dc0894016d6cc061bfbd366eecad12e46773
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-02-07 22:55:17 +0530

    upto date

--- 06-.sh
+++ 06-.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "hello world"
+
+echo "vardhan: hai thadhiya how are you?"
+echo "thadhiya: fine vardhan,what about you ?"
+echo "vardhan: good,what are you doing?"
+echo "thadhiya: i am watching TV"
+
