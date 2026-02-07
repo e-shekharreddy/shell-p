df2256b447674506cfaba62824d547026f53a370
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-02-07 23:11:12 +0530

    upto date

--- 05-variables.sh
+++ 05-variables.sh
@@ -6,6 +6,6 @@ read USER_NAME
 echo "User name is $USER_NAME"
 
 echo "please enter password::"
-read -s PASSWORD # -e is for to hide the password 
+read -e PASSWORD # -e is for to hide the password 
 
 read "password is $PASSWORD" #to see what password we enter
