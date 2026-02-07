dc4574d6e11a9d85aa61a4e1e931f370ee51b298
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-02-08 00:55:26 +0530

    upto date

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -4,4 +4,11 @@ Num2=150
 
 sum=$(($Num1 + $Num2))
 
-echo "Sum of the numbers $sum"
+echo "Sum of the numbers: $sum"
+
+#array
+
+Fruits=("apple" "banana" "mango" "orange")
+
+echo "fruits are : $Fruits"
+echo "first fruit is :$(Fruits(0))"
