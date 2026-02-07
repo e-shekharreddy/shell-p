b1a19a6c1badc61affc3da701d3cb3b3ddd7563f
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-02-07 23:56:36 +0530

    upto date

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,5 +1,15 @@
 #!/bin?bash
 
-TIMESTAMP=$(date)
+# TIMESTAMP=$(date)
 
-echo "script executed at:$TIMESTAMP"
+# echo "script executed at:$TIMESTAMP"
+
+start_time=$(date +%s)
+
+sleep 10
+
+end_time=$(date +%s)
+
+Total_time=$(($end_time - $start_time))
+
+echo "Script executed $Total_time seconds"
