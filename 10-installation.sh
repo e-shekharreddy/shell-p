24279260b2fb288fd506a39470ce2b47629c7952
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-02-08 15:29:56 +0530

    update

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 USERID=$(id -u)
 
 if [$USERID -ne 0]; then
-    echo "Please run the script with root user access"
+    echo "Please run this script with root user access"
 if
 
 echo "installing nginx"
