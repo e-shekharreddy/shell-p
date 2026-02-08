b78aea4568c858cf5fb709a5d6cafb33bd4ff504
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-02-08 15:45:44 +0530

    update

--- 10-installation.sh
+++ 10-installation.sh
@@ -4,8 +4,9 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "Please run this script with root user access"
+    exit 1
 fi 
 
-echo "installing nginx"
-
+echo "installing Nginx"
 dnf install nginx -y 
+
