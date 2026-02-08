52aae56f2c2cbfefad9016b5f9c7ad9f91636457
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-02-08 14:41:22 +0530

    update

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -9,8 +9,8 @@ NUMBER=$1
 
 if [$NUMBER - gt 143]
 echo "given number greater than 143"
-elif [$NUMBER -lt 143]
-echo "given number less than 143"
+# elif [$NUMBER -lt 143]
+# echo "given number less than 143"
 else [$NUMBER -eq 143]
 echo "given number equals to 143"
 fi
