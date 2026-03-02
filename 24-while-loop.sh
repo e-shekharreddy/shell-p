1e7bcf44b609bf349a95c2561d3437e015d9c043
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-02 21:28:54 +0530

    updated

--- 24-while-loop.sh
+++ 24-while-loop.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-count=1
-while [ $count -lt 6 ]
-do
-    echo "Count is $count"
+# count=1
+# while [ $count -lt 6 ]
+# do
+#     echo "Count is $count"
 
-    sleep 1 
-    #increment count
-    ((count++))
-done 
+#     sleep 1 
+#     #increment count
+#     ((count++))
+# done 
+
+while IFS= read -r line;  #IFS = internal field separator
+do
+    #process each line 
+    echo "$line"
+
+done < 23-delete_old_logs.sh  #which fileto read
