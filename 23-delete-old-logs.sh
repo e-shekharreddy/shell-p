8d074593b8ce7526f6a677c187bd2af1c3845942
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-02 22:01:56 +0530

    updated

--- 23-delete-old-logs.sh
+++ 23-delete-old-logs.sh
@@ -9,14 +9,27 @@ N="\e[0"
 LOGS_DIR=/home/ec2-user/app-logs
 LOGS_FILE="$LOGS_DIR/$0.log"
 
+if [ ! -d $LOGS_DIR ]; then
+    echo -e "$LOGS_DIR does not exitst"
+    exit 1
+fi 
+
+
+
 FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -type f -mtime +14)
-# echo "$FILES_TO_DELETE"
+echo "$FILES_TO_DELETE"
 
 while IFS= read -r filepath;
 do 
     echo "Deleting file: $filepath"
     rm -f $filepath
     echo "Deleted file: $filepath"
-
+    
 done <<< $FILE_TO_DELETE    # here <<< for to read and get the varaible 
                             # if we use > to get the output
+
+
+                            #if [ ! -d $LOGS_DIR ]; then
+                            #   echo -e "$LOGS_DIR" DOES not EXITS
+                            #   exit 1
+                            # fi
