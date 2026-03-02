a92f93b8bd1057b1cad23ce525327241d60d0f79
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-02 16:40:42 +0530

    updated

---
+++ 21-script-1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+country=india
+
+echo "I am in : $coumtry"
+echo "PID of SCRIPT-1: $$"
---
+++ 22-script-2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+country=may_be
+
+echo "I am in :$country"
+echo "PID of SCRIPT-2: $$"
