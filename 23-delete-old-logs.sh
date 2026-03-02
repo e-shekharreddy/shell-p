4a5f71a87795228ea7252449bae074ad21c24501
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-02 20:51:15 +0530

    updated

---
+++ 23-delete-old-logs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33"
+B ="\e[34"
+N="\e[0"
+
+LOGS_DIR=/home/ec2-user/app-logs
+LOGS_FILE="$LOGS_DIR/$0.log"
+
+FILES_TO_DELETE=$("find $LOGS_DIR -name "*.log" -type f -mtime +14")
+echo "$FILES_TO_DELETE"
