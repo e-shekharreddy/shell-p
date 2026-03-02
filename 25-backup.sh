5856ce1b4ebdb5a3f9f03e51bfa23d928d6135ae
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-03 00:38:42 +0530

    updated

--- 25-backup.sh
+++ 25-backup.sh
@@ -12,7 +12,7 @@ N="\e[0m"
 
 SOURCE_DIR=$1
 DEST_DIR=$2
-DAY=${3: -14} #14 days default value if user does not provide 
+DAYS=${3: -14} #14 days default value if user does not provide 
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N"
@@ -25,18 +25,31 @@ USAGE(){
     echo -e "$R USAGE: sudo backup <source_dir> <dest_dir> <day[default 14 days] $N"
     exit 1
 } 
-
+log(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S")" | $1 | tee -a $LOGS_FILE
+}
 if [ $# -lt 2 ]; then
     USAGE
 fi
 
 if [ ! -d $SOURCE_DIR ]; then 
-    echo -e "$R SOURCE_DIR does not exist $N"
+    echo -e "$R Source directory: SOURCE_DIR does not exist $N"
     exit1 1
 fi
 
 if [ ! -d $DEST_DIR ]; then
-    echo -e "$R DEST_DIR does not exist $N"
+    echo -e "$R Destination directory: DEST_DIR does not exist $N"
     exit 1
 fi
 
+# find the files
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime $DAYS)
+
+log "Backup started"
+log "Source Directory: $SOURCE_DIR"
+log "Destination Directory: $DEST_DIR"
+log "Days: $DAYS"
+
+if [ -z $FILES ]; then
+    log "No files to archieve... $Y SKIPPING $N"
+fi
