10290115e5ac39e43c2ae764dfd7cb3e2e7314ef
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-02 21:50:10 +0530

    updated

--- 23-delete-old-logs.sh
+++ 23-delete-old-logs.sh
@@ -10,4 +10,13 @@ LOGS_DIR=/home/ec2-user/app-logs
 LOGS_FILE="$LOGS_DIR/$0.log"
 
 FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -type f -mtime +14)
-echo "$FILES_TO_DELETE"
+# echo "$FILES_TO_DELETE"
+
+while IFS= read -r filepath;
+do 
+    echo "Deleting file: $filepath"
+    rm -f $filepath
+    echo "Deleted file: $filepath"
+
+done <<< $FILE_TO_DELETE    # here <<< for to read and get the varaible 
+                            # if we use > to get the output
--- 24-while-loop.sh
+++ 24-while-loop.sh
@@ -16,3 +16,11 @@ do
     echo "$line"
 
 done < 23-delete-old-logs.sh  #which fileto read
+
+
+while IFS= read -r filepath;
+do 
+    echo "$filepath"
+done <<< $FILE_TO_DELETE    # here <<< for to read and get the varaible 
+                            # if we use > to get the output
+                            
