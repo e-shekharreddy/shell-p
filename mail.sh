dd3e3c0c30c5debbb4ba990092f4c0c05e21b582
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-03 21:16:23 +0530

    updated

--- mail.sh
+++ mail.sh
@@ -8,7 +8,7 @@ TO_TEAM=$6
 FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g')
 
 
-FINAL_MESSAGE=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SEVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g" template.html)  # if we put sed -i that is permanet change so dont use -i here and -e is expression
+FINAL_MESSAGE=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$FINAL_MESSAGE_BODY/g" template.html)  # if we put sed -i that is permanet change so dont use -i here and -e is expression
 
 {
 echo "To: $TO_ADDRESS"
